1d71db4eea8f9aa7597e062ad28546fb55d3da1a
worthless6942 <danhduy315alt@gmail.com>
2026-03-04 07:35:50 +0700

    add hello

--- import random.py
+++ import random.py
@@ -1,18 +1,1 @@
-import random
-
-# Generate a random number between 1 and 100
-secret_number = random.randint(1, 100)
-
-print("Welcome to the Number Guessing Game!")
-print("I am thinking of a number between 1 and 100.")
-
-while True:
-    guess = int(input("Enter your guess: "))
-
-    if guess < secret_number:
-        print("Too low! Try again.")
-    elif guess > secret_number:
-        print("Too high! Try again.")
-    else:
-        print("Congratulations! You guessed the number!")
-        break
+hello 
